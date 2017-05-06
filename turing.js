69d8e5e38a951edb52205b003dd72cd7263fed28
Alagunag29 <alagunag29@gmail.com>
2017-05-06 14:00:40 -0500

    Turing terminada

--- turing.js
+++ turing.js
@@ -1,41 +1,96 @@
 var palabra = [
-	{"valor": "a", "estado": 1},
-	{"valor": "b", "estado": 0},
-	{"valor": "a", "estado": 0},
-	{"valor": "b", "estado": 0},
-	{"valor": "b", "estado": 0}
-];
-
-var exp = [
-		{ "estInicial": "q1", "tokInicial": "a", "estFinal": "q1", "tokFinal": "a", "desplazamiento": "R"},
-		{ "estInicial": "q2", "tokInicial": "b", "estFinal": "q2", "tokFinal": "b", "desplazamiento": "R"},
-		{ "estInicial": "q1", "tokInicial": "b", "estFinal": "q2", "tokFinal": "a", "desplazamiento": "R"},
-		{ "estInicial": "q2", "tokInicial": "B", "estFinal": "q3", "tokFinal": "B", "desplazamiento": "L"},
-		{ "estInicial": "q2", "tokInicial": "a", "estFinal": "q2", "tokFinal": "a", "desplazamiento": "R"},
+	{
+	 "valor": "a" ,
+	 "estado": 1 
+	}
 ]
 
+var exp = [
+		{
+		"eInicial": "q1",
+		 "tInicial": "a", 
+		 "eFinal": "q1", 
+		 "tFinal": "a", 
+		 "desplazamiento": "R"
+		}
+]
+
+var BLANCA;
+var VECES = true;
 
 $(function(){
-	console.log(palabra);
+
 	botton();
-	contenido();
+
 });
 
 function botton(){
-	$('input[type=button]').on('click', function(e){
+	$('#expresion').on('click', function(e){
 		e.preventDefault();
-		logica();	
+		addExpresion();
+	});
+
+	$('#blanca').on('click', function(e){
+		e.preventDefault();
+		addBlanca();
+	});
+
+	$('#inicial').on('click', function(e){
+		e.preventDefault();
+		addInicial();
+		contenido();
+	});
+
+	$('#simulacion').on('click', function(e){
+		e.preventDefault();
+		simulacion();
 	});
 }
 
+function addExpresion(){
+	var text = document.getElementById("text-expresion");
+	var tx	 = text.value;
+	var expreg = new RegExp('[(][q][0-9][,][a-z|A-Z][)][=][(][q][0-9][,][a-z|A-Z][,][R|L][)]')
+	if (expreg.test(text.value)){
+		exp.push({
+			eInicial: tx[1]+tx[2],
+			tInicial: tx[4],
+			eFinal: tx[8]+tx[9],
+			tFinal: tx[11],
+			desplazamiento: tx[13]
+		});
+		pintarExpresion('#contenedor-expresion', tx);
+	}
+	text.value = "";
+	console.log(exp);
+}
+
+function addBlanca(){
+	var blanca = document.getElementById("text-blanca");
+	BLANCA = blanca.value;
+	pintarExpresion('#contenedor-blanca', blanca.value);
+	blanca.value = "";
+}
+
+
+function addInicial(){
+	est = 1;	
+	var inicial = document.getElementById("text-inicial");
+	for(var j=0; j < inicial.value.length; j++){
+		palabra.push({valor: inicial.value[j], estado: est});
+		est = 0;		
+	}
+	pintarExpresion('#contenedor-inicial', inicial.value);
+	inicial.value = ""
+}
 
 function contenido(){
 	var clase;
-	for(var i = 0; i < palabra.length; i++){
+	for(var i = 1; i < palabra.length; i++){
 		if(palabra[i].estado == 1 ){
-			clase = "<div class='turing-container turing-container-red'>" + "<a>" + palabra[i].valor + "</a>"+ "</div>";
+			clase = "<div class='turing-container purple'>" + "<a>" + palabra[i].valor + "</a>"+ "</div>";
 		}else {
-			clase = "<div class='turing-container turing-container-green'>" + "<a>" + palabra[i].valor + "</a>"+ "</div>";
+			clase = "<div class='turing-container blue'>" + "<a>" + palabra[i].valor + "</a>"+ "</div>";
 		}
 		
 		$('#tape-turing').append(
@@ -48,36 +103,42 @@ function remove(){
 	$('.turing-container').remove();
 }
 
-timer = setTimeout('temporizador()', 4000);
-function logica(){
+function pintarExpresion(lugar, pal){
+	$(lugar).append('<p>'+ pal +'</p>');
+}
+
+function simulacion(){
 	var estado = "q1";
-	var position = 0;
-	palabra[0].estado = 1; palabra[0].valor = "a";
-	palabra[1].estado = 0; palabra[1].valor = "b";
-	palabra[2].estado = 0; palabra[2].valor = "a";
-	palabra[3].estado = 0; palabra[3].valor = "b";
-	palabra[4].estado = 0; palabra[4].valor = "b";
-	for(var i =0 ; i < exp.length; i++){
-		sw = false;
-		for(var j = 0; j <  palabra.length && sw == false; j++){
-			if(estado == exp[j].estInicial && palabra[position].valor == exp[j].tokInicial ){
-				sw = true;
-				palabra[position].estado = 0;
-				palabra[position].valor  = exp[j].tokFinal;
-				estado = exp[j].estFinal;
-				if(exp[j].desplazamiento == "R"){
-					position++;
-				}else{
-					position--;
+	var sw = true;
+	pos = 1;
+	if(VECES == true){
+		for(var i = 1; i < palabra.length ; i++){
+			var veces = 0;
+			for(var j = 1; j < exp.length; j++ ){
+				if( (estado == exp[j].eInicial) && (palabra[i].valor == exp[j].tInicial) && (pos >= 1) ){
+					veces++;
+					palabra[pos].estado = 0;
+					palabra[pos].valor  = exp[j].tFinal;
+				    estado = exp[j].eFinal;
+					if(exp[j].desplazamiento == "R"){
+						pos++;
+					}else{
+						pos--;
+					}	
+					if (pos >= 1 && pos < palabra.length){
+						palabra[pos].estado = 1;
+					}else{
+					 	palabra[pos-1].estado = 1;
+					}
 				}
-				if (position >= 0 && position < palabra.length){
-					palabra[position].estado = 1;
-				}else{
-					 palabra[position-1].estado = 1;
-				}				
-			}			
+			}
+			if(veces == 0){
+				sw = false;
+			}
 		}
 		remove();
 		contenido();
+		VECES = false;
+		console.log("termino");
 	}
 }
